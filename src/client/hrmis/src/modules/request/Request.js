4fb58704a97796c2bd03ce4ef0eb60ab7f1e0e6a
Terrence Calzada <terrencecalzada@gmail.com>
2021-08-22 23:18:14 +0800

    Commit for issue  #2 Nav and Side bar

---
+++ src/client/hrmis/src/modules/request/Request.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function RequestMainView(props){
+    return (
+        <h1 className="margin-left-2 margin-top-2">Request</h1>
+    );
+}
+
+export default RequestMainView
