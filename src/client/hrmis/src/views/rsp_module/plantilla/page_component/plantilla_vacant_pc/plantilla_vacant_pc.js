8efe4ff34064e3b86a9f94fbc872d16a9ff9f0cf
ledz <legeeictisd@gmail.com>
2022-03-09 14:29:01 +0800

    fixed vacant position page

--- src/client/hrmis/src/views/rsp_module/plantilla/page_component/plantilla_vacant_pc/plantilla_vacant_pc.js
+++ src/client/hrmis/src/views/rsp_module/plantilla/page_component/plantilla_vacant_pc/plantilla_vacant_pc.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useMemo, useState } from "react";
+import React, { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import BreadcrumbComponent from "../../../../common/breadcrumb_component/Breadcrumb";
 import IconComponent from "../../../../common/icon_component/icon";
 import { API_HOST } from "../../../../../helpers/global/global_config";
@@ -13,6 +13,12 @@ import { useTable, useSortBy, useGlobalFilter, useFilters } from "react-table";
 import axios from "axios";
 import { AiFillPrinter } from "react-icons/ai";
 import { plantillaVacantBreadCramp } from "../../static/breadcramp_data";
+import { useToggleService } from "../../../../../services/toggle_service";
+import { useDispatch } from "react-redux";
+import NextInRankModal from "../next_in_rank_modal/next_in_rank_modal";
+import { plantillaItemMenuItem } from "../../static/menu_items";
+import { MdMoreHoriz } from "react-icons/md";
+import DropdownViewComponent from "../../../../common/dropdown_menu_custom_component/Dropdown_view";
 // eslint-disable-next-line
 // import { BreadCrumbsData } from "../../static/breadcramp_data";
 
@@ -49,140 +55,184 @@ const PlantillaItemsVacantPositionComponentView = () => {
             textHelper="Close Vacant Position"
           />
         </div>
-        <PlantillaDataTableDisplay type={0} />
+        <div>
+          <TableView regularValue={1} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const TableView = (props) => {
+  let [toggleNextRank, setToggletoggleNextRank] = useToggleService(false);
+  let [toggleAddPlantillaItem, setTogglePlantillaItem] =
+    useToggleService(false);
+
+  const [buttonToggleState, setButtonToggleState] = useState({
+    on: false,
+    index: 0,
+  });
+
+  const buttonTogleTab = (number) => {
+    if (buttonToggleState.on === false) {
+      setButtonToggleState({ on: true, index: number });
+    } else {
+      setButtonToggleState({ on: false, index: number });
+    }
+  };
+
+  //API CALL FOR VIEWING
+  const dispatch = useDispatch();
+  const [plantillaItemTableData, setData] = useState();
+  useEffect(() => {
+    const plantillaItemApi = async () => {
+      await axios
+        .get(API_HOST + "plantilla-items")
+        .then((response) => {
+          setData(response.data.data);
+          console.log("Run");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    plantillaItemApi(plantillaItemTableData);
+    console.log();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <NextInRankModal
+        isDisplay={toggleNextRank}
+        onClose={setToggletoggleNextRank}
+      />
+      <div className="plantilla-table">
+        <div className="scrollable-div-table">
+          <table id="custom-table">
+            <thead>
+              <tr className="fixed-label-table">
+                <th>
+                  <button>
+                    <BsArrowUp />
+                  </button>{" "}
+                  Item No.
+                </th>
+                <th>
+                  <button>
+                    <BsArrowUp />
+                  </button>{" "}
+                  Position
+                </th>
+                <th>
+                  <button>
+                    <BsArrowUp />
+                  </button>{" "}
+                  Office
+                </th>
+                <th>
+                  <button>
+                    <BsArrowUp />
+                  </button>{" "}
+                  Status
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {plantillaItemTableData &&
+                plantillaItemTableData.map((data) => {
+                  if ((props.regularValue === 1) & (data.itm_regular === 1)) {
+                    return (
+                      <tr className="trClass" key={data.itm_id}>
+                        <td>{data.itm_no ?? ""}</td>
+                        <td>{data.position.pos_short_name ?? ""}</td>
+                        <td>{data.office.ofc_acronym ?? ""}</td>
+                        <td className="column-option">
+                          <div className="inline-div-td-1">
+                            {statusDisplay[data.itm_status]}
+                            <br />
+                            {}
+                          </div>
+                          <div className="inline-div-td-2">
+                            <button onClick={() => buttonTogleTab(data.itm_id)}>
+                              <MdMoreHoriz size="15" />
+                            </button>
+
+                            <DropdownViewComponent
+                              display={
+                                buttonToggleState.on
+                                  ? buttonToggleState.index === data.itm_id
+                                    ? "block"
+                                    : "none"
+                                  : "none"
+                              }
+                              itemList={plantillaItemMenuItem}
+                              onClick={() => {
+                                setToggletoggleNextRank();
+                                setButtonToggleState({
+                                  on: data.itm_id,
+                                  index: false,
+                                });
+                              }}
+                            />
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  } else if (
+                    (props.regularValue === 0) &
+                    (data.itm_regular === 0)
+                  ) {
+                    return (
+                      <tr className="trClass" key={data.itm_id}>
+                        <td>{data.itm_no}</td>
+                        <td>{data.position.pos_short_name}</td>
+                        <td>{data.office.ofc_acronym}</td>
+                        <td className="column-option">
+                          <div className="inline-div-td-1">
+                            {statusDisplay[data.itm_status]}
+                            <br />
+                            {}
+                          </div>
+                          <div className="inline-div-td-2">
+                            <button onClick={() => buttonTogleTab(data.itm_id)}>
+                              <MdMoreHoriz size="15" />
+                            </button>
+
+                            <DropdownViewComponent
+                              display={
+                                buttonToggleState.on
+                                  ? buttonToggleState.index === data.itm_id
+                                    ? "block"
+                                    : "none"
+                                  : "none"
+                              }
+                              itemList={plantillaItemMenuItem}
+                              onClick={() => {
+                                setToggletoggleNextRank();
+                                setButtonToggleState({
+                                  on: data.itm_id,
+                                  index: false,
+                                });
+                              }}
+                            />
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  }
+                  return <React.Fragment></React.Fragment>;
+                })}
+            </tbody>
+          </table>
+        </div>
+
+        <p className="data-entry">
+          Total of {plantillaItemTableData && plantillaItemTableData.length}{" "}
+          Entries
+        </p>
+        <br />
       </div>
     </React.Fragment>
   );
 };
 export default PlantillaItemsVacantPositionComponentView;
-
-export const PlantillaDataTableDisplay = ({ type }) => {
-  const [plotData, setPlotData] = useState([]);
-
-  const plantillaItemApi = async () => {
-    await axios
-      .get(API_HOST + "vacantpositions/" + type)
-      .then((response) => {
-        let data = response.data.data ?? [];
-        let dataPlot = [];
-
-        data?.forEach((element) => {
-          dataPlot.push({
-            itm_no: element.itm_no,
-            pos_short_name: element.position.pos_short_name,
-            ofc_acronym: element.office.ofc_acronym,
-            itm_status: statusDisplay[element.itm_status],
-            pos_category: element.position.pos_category,
-          });
-        });
-
-        setPlotData(dataPlot);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  useLayoutEffect(() => {
-    plantillaItemApi();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  let data = useMemo(() => plotData, [plotData]);
-
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Item No.",
-        accessor: "itm_no", // accessor is the "key" in the data
-      },
-      {
-        Header: "Position",
-        accessor: "pos_short_name",
-      },
-      {
-        Header: "Office",
-        accessor: "ofc_acronym",
-      },
-      {
-        Header: "Status",
-        accessor: "itm_status",
-      },
-      {
-        Header: "Category",
-        accessor: "pos_category",
-      },
-    ],
-    []
-  );
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useFilters,
-      useGlobalFilter,
-      useSortBy
-    );
-
-  return (
-    <React.Fragment>
-      <br />
-
-      <div className="default-table">
-        <table className="table-design" {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr
-                className="main-header"
-                {...headerGroup.getHeaderGroupProps()}
-              >
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    <span>
-                      {column.isSorted ? (
-                        column.isSortedDesc ? (
-                          <BsArrowDown />
-                        ) : (
-                          <BsArrowUp />
-                        )
-                      ) : (
-                        ""
-                      )}
-                    </span>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr className="trHoverBody" {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <p
-          style={{
-            fontSize: "small",
-            color: "rgba(70, 70, 70, 0.6)",
-            marginTop: "10px",
-          }}
-        >
-          Total of {rows.length} entries
-        </p>
-      </div>
-    </React.Fragment>
-  );
-};
