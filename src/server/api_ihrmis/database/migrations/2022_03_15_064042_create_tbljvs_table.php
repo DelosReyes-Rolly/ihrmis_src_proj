843edf1f592369a43fa1559f11f2cdc0847719a9
tezadaconnect <terrencecalzada@gmail.com>
2022-03-15 14:42:36 +0800

    Regenerate new migration files

--- src/server/api_ihrmis/database/migrations/2022_03_15_064042_create_tbljvs_table.php
+++ src/server/api_ihrmis/database/migrations/2022_03_15_064042_create_tbljvs_table.php
@@ -19,7 +19,7 @@ class CreateTbljvsTable extends Migration
             $table->unsignedTinyInteger('jvs_version')->comment('Indicates the version number of the form.');
             $table->string('jvs_min_com_desc')->comment('Minimum qualification standards for job comptency.');
             $table->string('jvs_prepared')->comment('Name of the person who prepared the form, the date he/she signed and the signature file if opted to be saved, in the format "<name>|<date>|<filename>".');
-            $table->string('jvs_approved')->comment('Name of the person who approved the form, the date he/she signed  and the signature file if opted to be saved, in the format "<name>|<date>|<filename>".');
+            $table->text('jvs_approved')->comment('Name of the person who approved the form, the date he/she signed  and the signature file if opted to be saved, in the format "<name>|<date>|<filename>".');
             $table->string('jvs_signed_file')->comment('Name of the generated PDF file including the path where it is located.');
         });
     }
