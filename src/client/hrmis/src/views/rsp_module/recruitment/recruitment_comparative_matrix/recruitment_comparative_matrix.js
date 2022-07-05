c8b8e51113ab0067072ffe114038c86730031b69
jcdampil23 <jcdampil23@gmail.com>
2022-07-05 10:38:59 +0800

    Removed Redundant files, and moved comparative matrix files to another folder

--- src/client/hrmis/src/views/rsp_module/recruitment/recruitment_comparative_matrix/recruitment_comparative_matrix.js
+++ src/client/hrmis/src/views/rsp_module/recruitment/recruitment_comparative_matrix/recruitment_comparative_matrix.js
@@ -3,13 +3,16 @@ import { ComparartiveBreadCrumbs } from '../static/breadcramp_item';
 import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_HOST } from '../../../../helpers/global/global_config';
-import RecruitmentComparativeTable from './page_tables/recruitment_compartative_matrix_table';
-import RecruitmentRatingAssessment from './page_tables/recruitment_rating_assestment_table';
+import RecruitmentComparativeTable from './page_tables.js/recruitment_compartative_matrix_table';
+import RecruitmentRatingAssessment from './page_tables.js/recruitment_rating_assestment_table';
+import { useIsMounted } from '../../../../helpers/use_hooks/isMounted';
 
 const RecruitmentComparativeMatrix = () => {
+	const mounted = useIsMounted();
 	const urlpath = window.location.pathname;
 	const route = urlpath.split('/');
 	const plantilla_id = route[5];
+	const applicant = route[6];
 	const [plantilla, setPlantilla] = useState();
 	const [applicantId, setApplicantId] = useState();
 	const [page, setPageType] = useState('');
@@ -18,11 +21,16 @@ const RecruitmentComparativeMatrix = () => {
 		await axios
 			.get(API_HOST + 'get-cm-detail/' + plantilla_id)
 			.then((response) => {
+				if (!mounted.current) return;
 				setPlantilla(response.data.data.plantilla);
 				setRequirements(response.data.data.requirements);
 			});
 	}, [plantilla_id]);
 	useEffect(() => {
+		if (applicant !== undefined) {
+			setApplicantId(applicant);
+			setPageType('ra');
+		}
 		getCMData();
 	}, [getCMData]);
 	return (
