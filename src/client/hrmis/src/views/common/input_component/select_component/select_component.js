90d996170a1872e3ae029d298a254828fecaed25
jcdampil23 <jcdampil23@gmail.com>
2022-07-05 10:45:11 +0800

    Routes and isMounted Hook

--- src/client/hrmis/src/views/common/input_component/select_component/select_component.js
+++ src/client/hrmis/src/views/common/input_component/select_component/select_component.js
@@ -1,46 +1,47 @@
-import React from "react";
+import React from 'react';
+import { BsListOl } from 'react-icons/bs';
 
 const SelectComponent = (props) => {
-  return (
-    <select
-      className="select-component"
-      id={props.id}
-      value={props.value}
-      style={{ marginTop: "3px" }}
-      onChange={props.onChange}
-      readOnly={props.readOnly}
-      name={props.name}
-    >
-      <option className="option-component default-option" value="">
-        {props.defaultTitle}
-      </option>
-      {props.itemList.map((item) => {
-        if (item.id === null) {
-          return (
-            <option
-              className="option-component"
-              key={item.name}
-              value={item.code}
-            >
-              {item.name}
-            </option>
-          );
-        } else {
-          return (
-            <option className="option-component" key={item.id} value={item.id}>
-              {item.title}
-            </option>
-          );
-        }
-      })}
-    </select>
-  );
+	return (
+		<select
+			className='select-component'
+			id={props.id}
+			value={props.value}
+			style={{ marginTop: '3px' }}
+			onChange={props.onChange}
+			readOnly={props.readOnly}
+			name={props.name}
+		>
+			<option className='option-component default-option' value=''>
+				{props.defaultTitle}
+			</option>
+			{props.itemList.map((item) => {
+				if (!item.hasOwnProperty('id')) {
+					return (
+						<option
+							className='option-component'
+							key={item.name}
+							value={item.code}
+						>
+							{item.name}
+						</option>
+					);
+				} else {
+					return (
+						<option className='option-component' key={item.id} value={item.id}>
+							{item.title}
+						</option>
+					);
+				}
+			})}
+		</select>
+	);
 };
 
 SelectComponent.defaultProps = {
-  defaultTitle: "Default",
-  itemList: [],
-  readOnly: false,
+	defaultTitle: 'Default',
+	itemList: [],
+	readOnly: false,
 };
 
 export default SelectComponent;
--- src/server/api_ihrmis/routes/api.php
+++ src/server/api_ihrmis/routes/api.php
@@ -16,7 +16,6 @@ use App\Http\Controllers\Applicant\TblapplicantTrainingsController;
 use App\Http\Controllers\Applicant\TblapplicantVoluntaryController;
 use App\Http\Controllers\Employee\EmployeeController;
 use App\Http\Controllers\Jvs\TbljvsController;
-use App\Http\Controllers\Library\CategoryGroup;
 use App\Http\Controllers\Library\DocumentRequirements;
 use App\Http\Controllers\Library\EvaluationBattery;
 use App\Http\Controllers\MailController;
@@ -44,7 +43,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('logout', [AuthController::class, "logout"]);
 });
- Route::get('getOffices', [TblofficesController::class, "getAllOffices"]);
+Route::get('getOffices', [TblofficesController::class, "getAllOffices"]);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
@@ -59,6 +58,7 @@ Route::get('verify-email', [TblapplicantProfileController::class, "verifyEmail"]
 Route::get('get-new-applicant/{id?}', [TblapplicantProfileController::class, "getApplicant"]);
 Route::get('get-new-family/{id}', [TblapplicantProfileController::class, "getFamilyChildren"]);
 Route::get('get-complete-applicant/{id}', [RecruitmentController::class, "getCompleteApplicantsProfile"]);
+
 //Reports
 Route::get('generate-POA/{plantillaId}', [RecruitmentController::class, "generatePOAReport"]);
 Route::get('generate-RAI/{month}/{year}', [RecruitmentController::class, "generateRAIReport"]);
@@ -168,7 +168,7 @@ Route::post('add-dty-items/{id}', [TblplantillaDtyAndRspnsbltyController::class,
 // OTHER RESOURCES
 //=======================================================================================
 Route::resource('plantilla-items', TblplantillaItemsController::class);
-Route::resource('offices', TblofficesController::class);
+Route::resource('offices', \TblofficesController::class);
 Route::post('save-agency', [TblofficesController::class, "saveAgency"]);
 Route::resource('positions', TblpositionsController::class);
 Route::resource('positions-csc-std', TblpositionsController::class);
@@ -176,9 +176,9 @@ Route::resource('positions-csc-std', TblpositionsController::class);
 //=======================================================================================
 //  Library Resources
 //=======================================================================================
-Route::resource('category-groups', CategoryGroup::class);
-Route::resource('documentary-requirements', DocumentRequirements::class);
-Route::resource('evaluation-battery', EvaluationBattery::class);
+Route::resource('category-groups', Library\CategoryGroup::class);
+Route::resource('documentary-requirements', Library\DocumentRequirements::class);
+Route::resource('evaluation-battery', Library\EvaluationBattery::class);
 Route::get('evaluation-battery/{grpID}/{sg}', [EvaluationBattery::class, "show"]);
 //=======================================================================================
 // AUTH END POINTS
@@ -187,7 +187,8 @@ Route::post('login', [AuthController::class, "login"]);
 Route::post('register', [AuthController::class, "register"]);
 
 
-
+Route::post('assessment-score', [RecruitmentController::class, 'saveAssessment']);
+Route::post('competency-assessment-score', [RecruitmentController::class, 'saveCompetencyAssessment']);
 
 //=======================================================================================
 // MAIL CONTROLLER ENDPOINTS
@@ -291,4 +292,3 @@ Route::delete('remove-emp_edu/{edu_id}', [EmployeeController::class, "removeEmpl
 // LIBRARY CONTROLLER ENDPOINTS
 //=======================================================================================
 Route::get('get-history-service/{id?}', [EmployeeController::class, "getEmployeeHistoryService"]);
-
