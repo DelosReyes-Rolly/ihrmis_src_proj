481924c09377893d995e7493bf7b0bd44d07235a
tezadaconnect <terrencecalzada@gmail.com>
2022-08-11 14:08:34 +0800

    Added some features

--- src/server/api_ihrmis/app/Http/Resources/Onboarding/NewAppointeesResource.php
+++ src/server/api_ihrmis/app/Http/Resources/Onboarding/NewAppointeesResource.php
@@ -19,7 +19,7 @@ class NewAppointeesResource extends JsonResource
             "emp_id" => $this->app_emp_id,
             "itm_id" => $this->app_itm_id,
             "name" => $this->employee->emp_nm_first." ".$this->employee->emp_nm_mid." ".$this->employee->emp_nm_last." ".$this->employee->emp_nm_extn,
-            "photo" => $this->employee->profile ?? null,
+            "photo" => $this->employee->profile->emp_photo ?? null,
             "position" => $this->plantillaItems->tblpositions->pos_title ?? "",
             "office" => $this->plantillaItems->tbloffices->ofc_acronym ?? "",
         ];
