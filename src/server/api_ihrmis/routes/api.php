f6cf095e9102202020508106b43f1261c1dcd862
DelosReyes-Rolly <rollydelosreyes729@gmail.com>
2022-10-28 17:05:28 +0800

    updates

--- src/server/api_ihrmis/routes/api.php
+++ src/server/api_ihrmis/routes/api.php
@@ -1,13 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-<<<<<<< HEAD
 use App\Http\Controllers\DevPlanPeriodsController;
 use App\Http\Controllers\PersonController;
-=======
-use App\Http\Controllers\PersonController;
 use App\Http\Controllers\RemarksController;
->>>>>>> 2215c9adb1b325f735f0ec810bc4a6a00ae0bf7f
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,9 +29,6 @@ Route::post('update-user', [AuthController::class, "updateUser"]);
 Route::get('get-person', [PersonController::class, "getPerson"]);
 Route::post('add-person', [PersonController::class, "addPerson"]);
 
-<<<<<<< HEAD
 Route::post('add-period', [DevPlanPeriodsController::class, "addPeriod"]);
-=======
 Route::get('get-remarks', [RemarksController::class, "getRemarks"]);
 Route::post('add-remarks', [RemarksController::class, "addRemarks"]);
->>>>>>> 2215c9adb1b325f735f0ec810bc4a6a00ae0bf7f
